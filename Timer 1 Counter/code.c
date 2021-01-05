08e4e07fa9b0ad546802ee5b34b0a908cb9f4db0
Guilherme Lopes <74101477+guilhermelopeseng@users.noreply.github.com>
2021-01-05 15:22:04 -0300

    Update code.c

--- Timer 1 Counter/code.c
+++ Timer 1 Counter/code.c
@@ -9,15 +9,14 @@
 //Função principal
 void main()
 {
-   setup_timer_1(T1_EXTERNAL|T1_CLK_OUT);
-   set_timer1(0);
-   output_low(PIN_D0); 
- 
+   setup_timer_1(T1_EXTERNAL|T1_CLK_OUT);			// Configurações do Timer1
+   set_timer1(0);									// Inicializa com 0
+   output_low(PIN_D0); 								// Set 0 em RD0 
    while(TRUE){
 	   
-	   if(get_timer1() == 2){
-		   set_timer1(0);
-		   output_toggle(PIN_D0);
+	   if(get_timer1() == 3){						// Após 3 pulsos entra no if
+		   set_timer1(0);							// Reinicia o Timer 1
+		   output_toggle(PIN_D0);					// Muda o estado do LED
 	   }
    	
    }
